20f5f07ede297c11ca83def6ab42a4e02fe7ba0d
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-07-06 12:48:19 -0400

    Completed on scroll reveal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,10 @@
 import MobileMenu from './Modules/MobileMenu';
+import RevealOnScroll from './Modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "80%");
+new RevealOnScroll($(".testimonial"), "65%");
 
 
 
