6a0f56f9557d932a38910cf07878f0b3d1c44936
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-07-07 13:10:30 -0400

    Completed fixed header behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import MobileMenu from './Modules/MobileMenu';
 import RevealOnScroll from './Modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "80%");
-new RevealOnScroll($(".testimonial"), "65%");
+new RevealOnScroll($(".testimonial"), "75%");
+var stickyHeader = new StickyHeader();
 
 
 
